fb69035409ac78276a8a3c43d8c57465748e15a1
warben <quirox01@gmail.com>
2023-06-24 19:57:59 -0300

    force true

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/server");
 const { conn } = require("./src/DB_connection");
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3000, () => {
     console.log("Server raised");
   });
